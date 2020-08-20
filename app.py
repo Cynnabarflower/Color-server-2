956baaa48096617d298b1a59bceb00639c39e0d6
Cynnabarflower <Cinnabarflower@gmail.com>
2020-08-21 02:00:18 +0300

    'release'

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__);
 
 @app.route("/bot", methods=["POST"])
 def response():
-    name = 'filename.txt'
+    name = dict(request.form)['name']
+    data = dict(request.form)['data']
+    pas = dict(request.form)['pass']
     f = open(name, 'w')
-    f.write('123')
+    f.write(data)
     f.close()
     ftp = ftplib.FTP("ftp.neuropsylab.com")
-    ftp.login("gamedata@neuropsylab.com", "xGA)Wj636(LQ")
+    ftp.login("gamedata@neuropsylab.com", pas)
     with open(name, 'rb') as f:
         ftp.storbinary('STOR '+name, f)
     ftp.close()
