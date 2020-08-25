33464389031298f056fad8bbb2e23af542d23ce4
Cynnabarflower <59529031+Cynnabarflower@users.noreply.github.com>
2020-08-25 04:56:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,19 @@ app = Flask(__name__);
 
 @app.route("/bot", methods=["POST"])
 def response():
-    name = dict(request.form)['name']
-    data = dict(request.form)['data']
-    f = open(name, 'w')
-    f.write(data)
-    f.close()
-    ftp = ftplib.FTP("ftp.neuropsylab.com")
-    ftp.login("gamedata@neuropsylab.com", 'xGA)Wj636(LQ')
-    with open(name, 'rb') as f:
-        ftp.storbinary('STOR '+name, f)
-    ftp.close()
+    try:
+        name = dict(request.form)['name']
+        data = dict(request.form)['data']
+        f = open(name, 'w')
+        f.write(data)
+        f.close()
+        ftp = ftplib.FTP("ftp.neuropsylab.com")
+        ftp.login("gamedata@neuropsylab.com", 'xGA)Wj636(LQ')
+        with open(name, 'rb') as f:
+            ftp.storbinary('STOR '+name, f)
+        ftp.close()
+    except Exception:
+        print('ooops')
     return 'Done'
 
 
